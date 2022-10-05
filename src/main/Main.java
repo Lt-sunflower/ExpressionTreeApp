765ea0986eaf9b681babfe42d31c614fbc20b368
Lt-sunflower <87886381+Lt-sunflower@users.noreply.github.com>
2022-10-05 22:49:03 +0800

    Improve time complexity for replace fix

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,8 @@
 package main;
 
 import java.util.Collections;
+import java.util.Dictionary;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeSet;
 
@@ -32,6 +34,14 @@ public class Main {
 		// 1.1 Replace int with char(construct HashMap)
 		Map<Character,Integer> numberMap = expressionTreeUtil.storeNumbers(input);
 		
+		Map<Integer, Character> reverseMap = new HashMap<>();
+		
+		// O(n), n=sizeof keyset
+		for (Character c:numberMap.keySet())
+		{
+			reverseMap.put(numberMap.get(c), c);
+		}
+		
 		// Encountered problem where when not sorted, numbers get replaced out of sequenced e.g. affected 10 when replacing 1 or affect -1 when replacing 1
 		// Fix is currently not the best complexity wise, but will try find a better way to make sequential replace
 		TreeSet<Integer> sortedSet = new TreeSet<>();
@@ -40,22 +50,29 @@ public class Main {
 		TreeSet<Integer> reverseSortedSet = new TreeSet<>(Collections.reverseOrder());
 		reverseSortedSet.addAll(numberMap.values());
 		
+		// Where i<0, smallest -> largest e.g. -10 then -1
 		for (Object value : sortedSet)
 		{
 			int i = (int) value;
 			if (i<0)
 			{
-				Character c = getKeyByValue(numberMap,value);
+				// getKeyByValue traverses whole hashMap, returns once found (1-to-1 match), worst case - O(n) for EACH key, whole for loop o(n^2) for worst case
+//				Character c = getKeyByValue(numberMap,value);
+				
+				// Using additional HashMap changes get to constant time O(1)
+				Character c = reverseMap.get(i);
 				input = input.replaceAll(value.toString(),c.toString());
 			} else break;
 		}
 		
+		// Where i>=0, largest -> smallest e.g. 10 then 1
 		for (Object value : reverseSortedSet)
 		{
 			int i = (int) value;
 			if (i>=0)
 			{
-				Character c = getKeyByValue(numberMap,value);
+//				Character c = getKeyByValue(numberMap,value);
+				Character c = reverseMap.get(i);
 				input = input.replaceAll(value.toString(),c.toString());
 			} else break;
 		}
@@ -94,14 +111,14 @@ public class Main {
 
 	}
 
-	private static Character getKeyByValue(Map<Character, Integer> numberMap, Object value) {
-	
-		for (Character c: numberMap.keySet())
-		{
-			if (numberMap.get(c) == value)
-				return c;
-		}
-		return null;
-	}
+//	private static Character getKeyByValue(Map<Character, Integer> numberMap, Object value) {
+//	
+//		for (Character c: numberMap.keySet())
+//		{
+//			if (numberMap.get(c) == value)
+//				return c;
+//		}
+//		return null;
+//	}
 
 }
